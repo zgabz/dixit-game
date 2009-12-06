81925a5316096834735959d95ec5b2fd1d622427
vlad2135 <vlad2135@2c53fa28-e1e6-11de-81d3-db62269da9c1>
2009-12-06 15:44:10 +0000

    fix

--- trunk/dixit.js
+++ trunk/dixit.js
@@ -3,5 +3,5 @@ function btnDo_onClick()
    var skypeLink = document.getElementById("skypeLink");
    var tbSkypeLogin = document.getElementById("tbSkypeLogin");
 
-   skypeLink.href="callto://"+tbSkypeLogin.innerHTML;
+   skypeLink.href="callto://"+tbSkypeLogin.value;
 }
